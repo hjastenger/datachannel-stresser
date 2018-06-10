354cc6a07ce9e72efe04762d91faa88adf735fba
Hector Stenger <github@orti.works>
2018-06-10 13:24:10 +0200

    feat(cli): define default values and add parsed options from cli to conf

--- index.js
+++ index.js
@@ -10,28 +10,35 @@ program
     .option('-m, --messages <n>', 'number of messages to be send')
     .option('-i, --interval <n>', 'interval in which the messages are send')
     .option('-o, --ordered <b>', 'specify the DataChannel to be ordered or not')
+    .option('-cc, --concurrent <b>', 'specify the amount of concurrent connections per process')
     .option('-rtx, --retransmit-times <n>', 'specify number of retransmission times on each message')
     .option('-rto, --retransmit-timeout <n>', 'specify miliseconds before dropping a message')
 ;
 
 program.parse(process.argv);
 
-function onResult(i, conf) {
-    const responseTimes = i.map((_) => (_.time_acquired - _.time_send));
-    return Promise.all(responseTimes.map((res) => {
-        return postData(`response_time,protocol=${conf.protocol},concurrency=${conf.concurrent_connections},messages=${conf.messages},interval=${conf.interval} value=${res}`)
-    }));
-}
-
-const configuration = {
-    url: "http://localhost:9000/empty",
+const defaultConfiguration = {
+    url: "http://localhost:9000",
     ws_url: "ws://localhost:9000/websocket",
     concurrent_connections: 1,
     messages: 1,
     interval: 300,
-    ordered: false,
+    ordered: true,
     maxRetransmit: 0,
-    payload: { data: "working" }
+    payload: {}
+};
+
+
+
+const configuration = {
+    url: program.url || defaultConfiguration.url,
+    ws_url: program.wsUrl || defaultConfiguration.ws_url,
+    concurrent_connections: program.concurrent || defaultConfiguration.concurrent_connections,
+    messages: program.messages || defaultConfiguration.messages,
+    interval: program.interval || defaultConfiguration.interval,
+    ordered: program.ordered || defaultConfiguration.ordered,
+    maxRetransmit: program.retransmitTimes || defaultConfiguration.maxRetransmit,
+    payload: defaultConfiguration.payload
 };
 
 const hooks = {
@@ -39,6 +46,13 @@ const hooks = {
 };
 
 dataChannelConnection(configuration, hooks);
+
+function onResult(i, conf) {
+    const responseTimes = i.map((_) => (_.time_acquired - _.time_send));
+    return Promise.all(responseTimes.map((res) => {
+        return postData(`response_time,protocol=${conf.protocol},concurrency=${conf.concurrent_connections},messages=${conf.messages},interval=${conf.interval} value=${res}`)
+    }));
+}
 
 function postData(payload) {
     const binary = Buffer.from(payload);
