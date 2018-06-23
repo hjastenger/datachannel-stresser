9de4cd8fd88ee2c50e7892b0ba5ee3386d364793
Hector Stenger <github@orti.works>
2018-06-23 14:16:28 +0200

    feat(bash): add bashscript runners so reboot is forced after experiment

---
+++ second_egress_ingress_run.sh
@@ -0,0 +1,112 @@
+rebootServer() {
+	echo "Rebooting server."
+ 	ssh -t -i ~/.ssh_do root@167.99.36.97 "source /etc/profile; tmux send-keys -t application 'C-c' Enter; reboot"
+	sleep 1m
+}
+
+startApplication() {
+	echo "Starting application"
+	ssh -t -i ~/.ssh_do root@167.99.36.97 "source /etc/profile;. /root/scripts/start_tmux.sh"
+	sleep 1m
+}
+
+setPacketLoss() {
+	echo "Setting packet loss"
+	ssh -t -i ~/.ssh_do root@167.99.36.97 "source /etc/profile;. /root/scripts/set_packet_loss.sh \"$1\""
+}
+
+setOutboundLoss() {
+	echo "Setting packet loss"
+	ssh -t -i ~/.ssh_do root@167.99.36.97 "source /etc/profile;. /root/scripts/set_outbound_loss.sh \"$1\""
+}
+
+setInboundLoss() {
+	echo "Setting packet loss"
+	ssh -t -i ~/.ssh_do root@167.99.36.97 "source /etc/profile;. /root/scripts/set_inbound_loss.sh \"$1\""
+}
+
+# Start Reliable Connection in Reliable Environment
+rebootServer
+setOutboundLoss 2.5%
+startApplication
+
+echo "done starting application, starting stress session"
+
+# Outbound retransmits
+node index.js -u http://10.133.58.193:9000/empty -w ws://10.133.58.193:9000/websocket --messages=480 --interval=500 --ordered=false  --retransmit-times=0 --concurrent=1 --optional=SecondRoundUnreliableDC2.5Outbound --protocol=datachannel --repeat=15
+
+rebootServer
+setOutboundLoss 2.5%
+startApplication
+
+node index.js -u http://10.133.58.193:9000/empty -w ws://10.133.58.193:9000/websocket --messages=480 --interval=500 --ordered=false  --retransmit-times=1 --concurrent=1 --optional=SecondRoundUnreliableDC2.5Outbound --protocol=datachannel --repeat=15
+
+rebootServer
+setOutboundLoss 2.5%
+startApplication
+
+node index.js -u http://10.133.58.193:9000/empty -w ws://10.133.58.193:9000/websocket --messages=480 --interval=500 --ordered=false  --retransmit-times=2 --concurrent=1 --optional=SecondRoundUnreliableDC2.5Outbound --protocol=datachannel --repeat=15
+
+echo "done starting application, starting stress session"
+
+rebootServer
+setOutboundLoss 2.5%
+startApplication
+
+# Outbound retransmitTimes
+node index.js -u http://10.133.58.193:9000/empty -w ws://10.133.58.193:9000/websocket --messages=480 --interval=500 --ordered=false  --retransmit-timeout=0 --concurrent=1 --optional=SecondRoundUnreliableDC2.5Outbound --protocol=datachannel --repeat=15
+
+rebootServer
+setOutboundLoss 2.5%
+startApplication
+
+node index.js -u http://10.133.58.193:9000/empty -w ws://10.133.58.193:9000/websocket --messages=480 --interval=500 --ordered=false  --retransmit-timeout=500 --concurrent=1 --optional=SecondRoundUnreliableDC2.5Outbound --protocol=datachannel --repeat=15
+
+rebootServer
+setOutboundLoss 2.5%
+startApplication
+
+node index.js -u http://10.133.58.193:9000/empty -w ws://10.133.58.193:9000/websocket --messages=480 --interval=500 --ordered=false  --retransmit-timeout=1000 --concurrent=1 --optional=SecondRoundUnreliableDC2.5Outbound --protocol=datachannel --repeat=15
+
+# Done Outbound
+
+# Inbound retransmits
+rebootServer
+setInboundLoss 2.5%
+startApplication
+
+node index.js -u http://10.133.58.193:9000/empty -w ws://10.133.58.193:9000/websocket --messages=480 --interval=500 --ordered=false  --retransmit-times=0 --concurrent=1 --optional=SecondRoundUnreliableDC2.5Inbound --protocol=datachannel --repeat=15
+
+rebootServer
+setInboundLoss 2.5%
+startApplication
+
+node index.js -u http://10.133.58.193:9000/empty -w ws://10.133.58.193:9000/websocket --messages=480 --interval=500 --ordered=false  --retransmit-times=1 --concurrent=1 --optional=SecondRoundUnreliableDC2.5Inbound --protocol=datachannel --repeat=15
+
+rebootServer
+setInboundLoss 2.5%
+startApplication
+
+node index.js -u http://10.133.58.193:9000/empty -w ws://10.133.58.193:9000/websocket --messages=480 --interval=500 --ordered=false  --retransmit-times=2 --concurrent=1 --optional=SecondRoundUnreliableDC2.5Inbound --protocol=datachannel --repeat=15
+
+echo "done starting application, starting stress session"
+
+# Inbound retransmitTimes
+
+rebootServer
+setInboundLoss 2.5%
+startApplication
+
+node index.js -u http://10.133.58.193:9000/empty -w ws://10.133.58.193:9000/websocket --messages=480 --interval=500 --ordered=false  --retransmit-timeout=0 --concurrent=1 --optional=SecondRoundUnreliableDC2.5Inbound --protocol=datachannel --repeat=15
+
+rebootServer
+setInboundLoss 2.5%
+startApplication
+
+node index.js -u http://10.133.58.193:9000/empty -w ws://10.133.58.193:9000/websocket --messages=480 --interval=500 --ordered=false  --retransmit-timeout=500 --concurrent=1 --optional=SecondRoundUnreliableDC2.5Inbound --protocol=datachannel --repeat=15
+
+rebootServer
+setInboundLoss 2.5%
+startApplication
+
+node index.js -u http://10.133.58.193:9000/empty -w ws://10.133.58.193:9000/websocket --messages=480 --interval=500 --ordered=false  --retransmit-timeout=1000 --concurrent=1 --optional=SecondRoundUnreliableDC2.5Inbound --protocol=datachannel --repeat=15
